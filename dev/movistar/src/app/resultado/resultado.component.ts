cc23db6b9a74d842c619c1dd40e46ec86caa2371
PALENGE <palenge@gmail.com>
2021-03-20 15:16:06 +0100

    resultado

--- dev/movistar/src/app/resultado/resultado.component.ts
+++ dev/movistar/src/app/resultado/resultado.component.ts
@@ -7,18 +7,18 @@ import { PromoService } from 'src/services/promocion/promo.service';
 @Component({
     selector: 'app-resultado',
     templateUrl: './resultado.component.html',
-    styleUrls: ['./resultado.component.sass'],
+    styleUrls: ['./resultado.component.scss'],
 })
 export class ResultadoComponent implements OnInit {
-    public promo$: Observable<Resultado[]> | null = null;
+    public promo: Observable<Resultado[]> | null = null;
 
     constructor(private promoservice: PromoService) {}
 
     ngOnInit(): void {
-        this.promo$ = this.promoservice.getResultadoByPromoId(
+        this.promo = this.promoservice.getResultadoByPromoId(
             environment.promo
         );
 
-        this.promo$.subscribe();
+        this.promo.subscribe();
     }
 }
