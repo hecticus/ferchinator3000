80548883c6a188816f8c0cf459a7f119631f1e59
PALENGE <palenge@gmail.com>
2021-04-29 22:48:02 +0200

    no message

--- dev/backend/app/controllers/PremiacionController.java
+++ dev/backend/app/controllers/PremiacionController.java
@@ -24,7 +24,7 @@ public class PremiacionController  extends Controller {
 
     public Result get(int promocion) {
         Promocion res = premiacionesService.getResultadoByPromoId(promocion);
-        return ok(res.getPremiacion() ==null? "{}": res.getPremiacion()).as("application/json");
+        return ok(res.getPremiacion() ==null? "[]": res.getPremiacion()).as("application/json");
     }
 
     public Result update(Http.Request request, int promocion) {
